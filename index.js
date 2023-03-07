51de4e45d4b0e92e27bca980868fba694749515c
bclbrendan <bclbrendan@gmail.com>
2023-03-07 19:06:32 +0000

    adjusted slight mistake in Manager.js after failing test, now passing all tests.

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
+const questions = [
+
+    {
+    type: 'input',
+    name: 'name',
+    message: "what is the Name",
+    },
+    {
+    type: 'input',
+    name: 'id',
+    message: "what is the ID",
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: "what is their email",
+    },
+]
+
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 
